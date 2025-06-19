3c89f717e7a59cded212b6fabe0fcc3fca5553f4
vivimcdermott123 <vivimcdermott@gmail.com>
2025-06-19 18:16:54 +0200

    Update startJuliaITensor.jl

--- startJuliaITensor.jl
+++ startJuliaITensor.jl
@@ -1,13 +1,2 @@
-#creatingnewfolder
-mkdir ~/itensor_test
-cd ~/itensor_test
-#startjuliainfolder
 julia
-#activate/installITensor
-import Pkg
-Pkg.activate(".")           # activate local project
-Pkg.add("ITensors")         # install ITensors cleanly
-#tryloading
-using ITensors
-@which MPS
-
+using ITensors, ITensorMPS
